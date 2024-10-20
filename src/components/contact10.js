cf368677db3d04c68ee1f4e39e0c23e71c15fee8
Jesus Cotrina <xt8424@rimac.com.pe>
2024-10-20 09:42:55 -0500

    .

---
+++ src/components/contact10.js
@@ -0,0 +1,140 @@
+import React, { Fragment } from 'react'
+
+import PropTypes from 'prop-types'
+
+import './contact10.css'
+
+const Contact10 = (props) => {
+  return (
+    <div className="contact10-container1 thq-section-padding">
+      <div className="contact10-max-width thq-section-max-width">
+        <div className="contact10-content1 thq-flex-row">
+          <div className="contact10-content2">
+            <h2>
+              {props.heading1 ?? (
+                <Fragment>
+                  <h2 className="contact10-text18 thq-heading-2">
+                    ¿Tienes alguna pregunta o sugerencia?
+                  </h2>
+                </Fragment>
+              )}
+            </h2>
+            <p>
+              {props.content1 ?? (
+                <Fragment>
+                  <p className="contact10-text16 thq-body-large">
+                    ¡Contáctanos!
+                  </p>
+                </Fragment>
+              )}
+            </p>
+          </div>
+        </div>
+        <div className="contact10-content3 thq-flex-row">
+          <div className="contact10-container2">
+            <img
+              alt={props.location1ImageAlt}
+              src={props.location1ImageSrc}
+              className="contact10-image1 thq-img-ratio-16-9"
+            />
+            <h3>
+              {props.location1 ?? (
+                <Fragment>
+                  <h3 className="contact10-text20 thq-heading-3">
+                    contacto@minimalistatodolist.com
+                  </h3>
+                </Fragment>
+              )}
+            </h3>
+            <p>
+              {props.location1Description ?? (
+                <Fragment>
+                  <p className="contact10-text17 thq-body-large">
+                    Estamos disponibles las 24 horas del día, los 7 días de la
+                    semana para responder tus consultas.
+                  </p>
+                </Fragment>
+              )}
+            </p>
+            <div className="contact10-container3">
+              <a
+                href="https://example.com"
+                target="_blank"
+                rel="noreferrer noopener"
+                className="thq-button-flat thq-body-small"
+              >
+                Get directions
+              </a>
+            </div>
+          </div>
+          <div className="contact10-container4">
+            <img
+              alt={props.location2ImageAlt}
+              src={props.location2ImageSrc}
+              className="contact10-image2 thq-img-ratio-16-9"
+            />
+            <h3>
+              {props.location2 ?? (
+                <Fragment>
+                  <h3 className="contact10-text21 thq-heading-3">
+                    Calle Principal, Ciudad Principal
+                  </h3>
+                </Fragment>
+              )}
+            </h3>
+            <p>
+              {props.location2Description ?? (
+                <Fragment>
+                  <p className="contact10-text19 thq-body-large">
+                    Puedes encontrarnos en nuestras oficinas centrales o
+                    contactarnos a través de nuestras redes sociales.
+                  </p>
+                </Fragment>
+              )}
+            </p>
+            <div className="contact10-container5">
+              <a
+                href="https://example.com"
+                target="_blank"
+                rel="noreferrer noopener"
+                className="thq-button-flat thq-body-small"
+              >
+                Get directions
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Contact10.defaultProps = {
+  content1: undefined,
+  location2ImageSrc:
+    'https://images.unsplash.com/photo-1660326310380-6c6404d40d9b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcyOTI5MzA5N3w&ixlib=rb-4.0.3&q=80&w=1080',
+  location1ImageSrc:
+    'https://images.unsplash.com/photo-1683107983270-38225df18859?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcyOTI5MzA5N3w&ixlib=rb-4.0.3&q=80&w=1080',
+  location1Description: undefined,
+  location2ImageAlt: 'Imagen de ubicación',
+  heading1: undefined,
+  location2Description: undefined,
+  location1ImageAlt: 'Icono de correo electrónico',
+  location1: undefined,
+  location2: undefined,
+}
+
+Contact10.propTypes = {
+  content1: PropTypes.element,
+  location2ImageSrc: PropTypes.string,
+  location1ImageSrc: PropTypes.string,
+  location1Description: PropTypes.element,
+  location2ImageAlt: PropTypes.string,
+  heading1: PropTypes.element,
+  location2Description: PropTypes.element,
+  location1ImageAlt: PropTypes.string,
+  location1: PropTypes.element,
+  location2: PropTypes.element,
+}
+
+export default Contact10
